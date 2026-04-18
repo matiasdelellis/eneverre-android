5899a5de0e544101f2e2ab01cf5381dd53749a1e
Matias De lellis <mati86dl@gmail.com>
2026-04-17 22:46:43 -0300

    Fix MStoRFC3339 and clean RFC3339toMS

--- app/src/main/java/ar/com/delellis/eneverre/util/Time.java
+++ app/src/main/java/ar/com/delellis/eneverre/util/Time.java
@@ -3,28 +3,27 @@ package ar.com.delellis.eneverre.util;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 public class Time {
     public static long RFC3339toMS(String dateTime) {
-        SimpleDateFormat timeServerFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
-        Date date = null;
+        SimpleDateFormat timeServerFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX", Locale.US);
         try {
-            date = timeServerFormatter.parse(dateTime);
+            timeServerFormatter.parse(dateTime);
         } catch (ParseException e) {
             return -1;
         }
-        timeServerFormatter.format(date);
 
         return timeServerFormatter.getCalendar().getTimeInMillis();
     }
 
     public static String MStoRFC3339(long time) {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX" , Locale.US);
         return simpleDateFormat.format(new Date(time));
     }
 
     public static String MStoFriendlyURL(long time) {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd_HHmmss");
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US);
         return simpleDateFormat.format(new Date(time));
     }
 }
